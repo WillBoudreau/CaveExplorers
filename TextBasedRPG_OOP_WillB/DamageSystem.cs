287aa4f0e269cc05610d54d0ad8fc3063561be8e
WillBoudreau <williamboudreau804@gmail.com>
2024-02-02 02:54:15 -0500

    redoing the Damage system class

--- TextBasedRPG_OOP_WillB/DamageSystem.cs
+++ TextBasedRPG_OOP_WillB/DamageSystem.cs
@@ -9,31 +9,6 @@ namespace TextBasedRPG_OOP_WillB
 {
     internal class DamageSystem
     {
-        Player player = new Player();
-        PlayerVals playerVals = new PlayerVals();
-        HealthSys healthSys = new HealthSys();
-        Enemy enemy = new Enemy();
-        public DamageSystem()
-        {
-            this.player = new Player();
-            this.playerVals = new PlayerVals();
-            this.healthSys = new HealthSys();
-            this.enemy = new Enemy();
-        }
-        public void Combat(int x, int y)
-        {
-            if(playerVals.Playerturn == true)
-            {
-                if ( player.x == enemy.x && player.y == enemy.y)
-                {
-                    healthSys.TakeDamage(healthSys.Attack);
-                }//<-- During the players trun Enemy 1 takes damage
-                if (player.x == enemy.x && player.y == enemy.y)
-                {
-                    player.x -= x;
-                    player.y -= y;
-                }//<-- Player cannot move on top of Enemy 1
-            }
-        }
+       
     }
 }
