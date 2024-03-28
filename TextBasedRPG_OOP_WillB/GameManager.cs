b5417cc2db02a98967d51207484b9fda68973fc0
WillBoudreau <williamboudreau804@gmail.com>
2024-03-28 13:47:49 -0400

    Revert "Restarting"
    This reverts commit 73f8be6cefdd89a9c4e894b89c79eb082ba184f9.

--- TextBasedRPG_OOP_WillB/GameManager.cs
+++ TextBasedRPG_OOP_WillB/GameManager.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,13 +14,17 @@ namespace TextBasedRPG_OOP_WillB
 
     internal class GameManager
     {
-        private Stopwatch stopwatch = new Stopwatch();
-        public SoundPlayer soundplayer;
+
         Map map;
         Player player;
-        Enemy enemy;
-        bool BossDead = false;
-        List<Enemy> enemies;
+        Chaser chaser;
+        Grunt grunt;
+        Runner runner;
+        Settings settings;
+        EnemyManager enemyMan;
+        List<EnemyManager> enemies;
+        EnemyManager enemyManager;
+        List<ItemManager> items;
         HUD hud;
         Generation generation;
         List<ItemManager> itemManager;
@@ -28,72 +32,30 @@ namespace TextBasedRPG_OOP_WillB
         //Achievements achievements;
         public GameManager()
         {
+            soundManager = new MusicManager();
             map = new Map();
-            player = new Player();
-            soundplayer = new SoundPlayer();
-            enemies = Enemy.GenerateEnenmies(5,2,2,1,map);
-            hud = new HUD(player, enemies);
+            enemies = new List<EnemyManager>();
+            items = new List<ItemManager>();
+            player = new Player(items,map);
+            hud = new HUD(player, enemies, map);
+            generation = new Generation();  
+            enemyMan = new EnemyManager(settings,player,items,map,enemies);
             Player.hud = hud;
             settings = new Settings();
             //achievements = new Achievements(player, enemies, itemManager);
         }
-        public void PlayMusic(string musicPath)
-        {
-            if(File.Exists(musicPath))
+        
+        
+            void GameInitialize()
             {
-                soundplayer.SoundLocation = musicPath;
-                soundplayer.Load();
-                soundplayer.PlayLooping();
+                soundManager.Init();
+                map.Init();
+                player.Init();
+                //generation.Init();
+                Intro();
+                enemyMan.Init();
             }
-            else
-            {
-                Console.Clear();
-                Console.WriteLine(musicPath + "not found");
-            }
-        }
-        void StarTimer()
-        {
-            stopwatch.Start();
-        }
-        void GenerateLlevel(int level)
-        {
-            if(level ==  1)
-            {
-                string musicPath = AppDomain.CurrentDomain.BaseDirectory + "kingdom-of-fantasy-version-60s-10817.wav";
-                PlayMusic(musicPath);
-                Console.CursorVisible = false;
-                map.MapArray();
-                map.ShowMap();
-            }
-        }
-        public void StopTimer()
-        {
-            stopwatch.Stop();
-            TimeSpan timeSpan = stopwatch.Elapsed;
-            string EndTime = String.Format("{0:00}:{1:00}:{2:00}",timeSpan.TotalHours,timeSpan.TotalMinutes,timeSpan.TotalSeconds);
-            Console.WriteLine("Level Completed in: " + EndTime );  
-        }
-        void Start()
-        {
-            Console.Clear();
-            Console.WriteLine("Welcome to Cave Explorers!");
-            Console.WriteLine("---------------------------");
-            Console.WriteLine("You goal adventurer is to defeat the Boss at the end of the level");
-            Console.WriteLine("Make sure you collect pickups to boost your stats along the way");
-            Console.WriteLine("---------------------------");
-            Console.WriteLine("On you adventure you will find many enemies and they will attack you");
-            Console.WriteLine("You can attack them by moving into them, but they can do the same and some may even try to escape!");
-            Console.WriteLine("---------------------------");
-            Console.WriteLine("Press any key to begin");
-            Console.ReadKey(true);
-            Console.Clear();
-        }
-        public void GameLoop()
-        {
-            StarTimer();
-            Start();
-            GenerateLlevel(1);
-            while (player.healthSys.normalHealth > 0)
+            void Update()
             {
                 //generation.Update();
                 player.Update(enemies,items,chaser,runner,grunt);
@@ -127,14 +89,12 @@ namespace TextBasedRPG_OOP_WillB
                 GameInitialize();
                 while (player.healthSys.normalHealth > 0)
                 {
-                    enemy.DisplayEnemy();
-                    enemy.EnemyMove(player,enemies);
-                    if(enemy.enemType == EnemType.Boss && enemy.healthSys.IsAlive == false)
-                    {
-                        BossDead = true;
-                        Win();
-                        break;
-                    }
+                   Update();
+                   Draw();
+                }
+                if (player.healthSys.normalHealth <= 0)
+                {
+                    GameOver();
                 }
             }
             //Game Over to game
@@ -146,20 +106,4 @@ namespace TextBasedRPG_OOP_WillB
                 Console.ReadKey();
             }
         }
-        public void GameOver()
-        {
-            Console.Clear();
-            Console.WriteLine("Game Over");
-            Console.WriteLine("You where defeated by: ");
-            Console.ReadKey();
-        }
-        public void Win()
-        {
-            Console.Clear();
-            StopTimer();
-            Console.WriteLine("You have defeated the boss! You win!");
-            Console.WriteLine("Press any key to quit");
-            Console.ReadKey();
-        }
     }
-}
--- TextBasedRPG_OOP_WillB/Player.cs
+++ TextBasedRPG_OOP_WillB/Player.cs
@@ -1,17 +1,6 @@
 using System;
 using System.Collections.Generic;
-<<<<<<< HEAD
 using System.Diagnostics;
-=======
-using System.Linq;
-using System.Net.Mime;
-using System.Runtime.InteropServices;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Xml.Schema;
->>>>>>> parent of 655fd0b (Began work Polymorphing the enemies)
 
 namespace TextBasedRPG_OOP_WillB
 {
@@ -21,7 +10,6 @@ namespace TextBasedRPG_OOP_WillB
         public bool Playerturn = true;
         public bool Attacked = false;
         public bool IsSlowed = false;
-<<<<<<< HEAD
         Chaser Chaser;
         Runner Runner;
         Grunt Grunt;
@@ -29,8 +17,6 @@ namespace TextBasedRPG_OOP_WillB
         List<ItemManager> items;
         NPCManager npc;
         public Stopwatch stopwatch = new Stopwatch();
-=======
->>>>>>> parent of 655fd0b (Began work Polymorphing the enemies)
         public static HUD hud;
         Settings settings;
         public int PlayerDamage;
@@ -344,7 +330,6 @@ namespace TextBasedRPG_OOP_WillB
                 Console.ReadKey();
                 Environment.Exit(0);
         }
-<<<<<<< HEAD
         //Starts the timer for the player
         public void StartTimer()
         {
@@ -360,9 +345,6 @@ namespace TextBasedRPG_OOP_WillB
 
         //Displays the player
         public void DisplayPlayer()
-=======
-         public void DisplayPlayer()
->>>>>>> parent of 655fd0b (Began work Polymorphing the enemies)
         {
             Console.SetCursorPosition(this.x, this.y);
             Console.ForegroundColor = ConsoleColor.Magenta;
